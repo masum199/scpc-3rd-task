84ff2fd90578f39b8a9c456a9e94916b659162b2
unknown <24abdullahalmasum@gmail.com>
2023-07-27 12:49:00 +0600

    done 7th task

--- index.js
+++ index.js
@@ -122,6 +122,25 @@
 // console.log(newPassword);
 
 
+// TASK 7
+
+function romanToInt(s) {
+    const map = {I: 1,V: 5,X: 10,L: 50,C: 100,D: 500,M: 1000,};
+    let result = 0;
+    s.split('').forEach((num, i) => {
+        if(map[num] < map[s[i + 1]]) {
+            result -= map[num];
+        }
+        else {
+            result += map[num]
+        }
+    });
+    return result;
+    };
+  // Test examples:
+  console.log(romanToInt("II"));
+
+
 
 
 
